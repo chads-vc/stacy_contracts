e4f4dd1b770b5a476a0102013a5e88138369f755
chad <chadsvc@protonmail.com>
2020-12-09 21:43:10 -0800

    debugged

--- scripts/deploy_fee_approver.js
+++ scripts/deploy_fee_approver.js
@@ -12,7 +12,7 @@ async function main() {
     const FeeApprover = await ethers.getContractFactory("FeeApprover");
     // constructor args STACY address, WETH address, uniswap factory address
     // rinkeby deploy args
-    const contract_obj = await Token.deploy("0xe86C3279e6732c4BE79Cb48C3E5b79A81492a8a5", "0xc778417e063141139fce010982780140aa0cd5ab", "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
+    const contract_obj = await FeeApprover.deploy("0xe86C3279e6732c4BE79Cb48C3E5b79A81492a8a5", "0xc778417e063141139fce010982780140aa0cd5ab", "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
   
     console.log("Address:", contract_obj.address);
 }
