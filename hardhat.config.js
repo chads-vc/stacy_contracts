51ae5779e015a893d779affc79de0824a58ce2cf
chad <chadsvc@protonmail.com>
2020-12-09 19:03:06 -0800

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+const { private_key, etherscan_api, infura_key } = require('./secret.json');
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.6.12",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infura_key}`,
+      accounts: [private_key]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: etherscan_api
+  }
+};
